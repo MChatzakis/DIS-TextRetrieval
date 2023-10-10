3d3a07ba7c80cb929cd3b922c5c1b0eea708790e
Manolis Chatzakis <manosxatzakiss2000@gmail.com>
2023-10-10 18:05:25 +0200

    model updates

--- src/models/IR_model.py
+++ src/models/IR_model.py
@@ -1,7 +1,12 @@
 class IR_model():
-    
     def __init__():
         pass
     
     def find_similar(self, query:str, topk=5, use_buildin=False):
+        pass
+    
+    def generate_submission_file():
+        pass
+    
+    def describe():
         pass
